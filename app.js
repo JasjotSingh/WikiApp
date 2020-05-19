e74abe55f213c1165237357051590d02bc5c1469
Jasjot <jasjot_jrarjn1@yahoo.in>
2020-05-19 14:37:31 -0700

    creating api's for put post patch get delete. for wiki microservice post

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 
 const app = express();
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -31,15 +31,144 @@ app.get("/", (req, res)=>{
   res.render("home");
 });
 
-app.get("/test", (req, res)=>{
-  articleModel.find({}, (err, articles)=>{
-    if(err)
-      console.log("Path: /test. \nError in find, :"+err);
-    else{
-      res.render("test", {articles: articles});
-    }
+app.route("/articles")
+  .get((req, res)=>{
+    articleModel.find({}, (err, articles)=>{
+      if(err){
+        console.log("Path: /test. \nError in find, :"+err);
+        res.send(err);
+      }
+      else{
+        res.send(articles);
+      }
+    });
+  })
+  .post((req, res)=>{
+    const postObj = new articleModel(
+                      {
+                          title: req.body.title,
+                          content: req.body.content
+                      }
+    );
+    console.log(req.body);
+    postObj.save((err)=>{
+      if(err){
+        console.log(err);
+        res.send(err);
+      }
+      else{
+        console.log("post saved:");
+        console.log(postObj);
+        res.send(postObj);
+      }
+    });
+  })
+  .delete((req, res)=>{
+    articleModel.deleteMany((err)=>{
+      if(err)
+        res.send(err);
+      else{
+        console.log("deleteing all articles");
+        res.send({message: "deleteing all articles"});
+      }
+    });
   });
-});
+
+app.route("/articles/:articleId")
+  .get((req, res)=>{
+    const articleId = req.params.articleId; //new mongoose.Types.ObjectId(req.params.articleId);
+
+    articleModel.findOne({"_id" : articleId}, (err, post)=>{
+      if(err)
+        res.send(err);
+      else{
+        res.send(post);
+      }
+    });
+  })
+  .put((req, res)=>{
+    const articleId = req.params.articleId;
+    const postUpdate = {
+                          title: req.body.title,
+                          content : req.body.content
+                        }; //can not use new articleMode() here, as it will create a object with new id.
+                        //and since we are updating the same object id has to be same.
+    articleModel.update({_id:articleId}, postUpdate, {overwrite : true}, (err, result)=>{
+      if(err)
+        res.send(err);
+      else
+        res.send(result);
+    });
+
+  })
+  .patch((req, res)=>{
+    const articleId = req.params.articleId;
+
+    articleModel.update({_id:articleId}, {$set : req.body}, (err, result)=>{
+      if(err)
+        res.send(err);
+      else
+        res.send(result);
+    });
+
+  })
+  .delete((req, res)=>{
+    const articleId = req.params.articleId;
+    articleModel.deleteOne({_id:articleId},(err)=>{
+      if(err)
+        res.send(err);
+      else
+        res.send({message : "article id :"+articleId+" deleted"});
+    });
+  });
+
+//=======IMP========//
+// can use route in case path is same, different from app.router
+//=======IMP========//
+
+// app.get("/articles", (req, res)=>{
+//   articleModel.find({}, (err, articles)=>{
+//     if(err){
+//       console.log("Path: /test. \nError in find, :"+err);
+//       res.send(err);
+//     }
+//     else{
+//       res.send(articles);
+//     }
+//   });
+// });
+//
+// app.post("/articles", (req, res)=>{
+//   const postObj = new articleModel(
+//                     {
+//                         title: req.body.title,
+//                         content: req.body.content
+//                     }
+//   );
+//   console.log(req.body);
+//   postObj.save((err)=>{
+//     if(err){
+//       console.log(err);
+//       res.send(err);
+//     }
+//     else{
+//       console.log("post saved:");
+//       console.log(postObj);
+//       res.send(postObj);
+//     }
+//   });
+// });
+//
+// app.delete("/articles", (req, res)=>{
+//   articleModel.deleteMany((err)=>{
+//     if(err)
+//       res.send(err);
+//     else{
+//       console.log("deleteing all articles");
+//       res.send({message: "deleteing all articles"});
+//     }
+//   });
+// });
 
 //=========================RESOURCES END======================//
 
